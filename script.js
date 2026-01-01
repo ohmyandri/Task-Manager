a22c66cbd7e14ba765dba1d9a3393a4e09de22b3
ohmyandri <120071660+ohmyandri@users.noreply.github.com>
2026-01-01 12:46:32 -0600

    Finished the modal, and also refactored some functions, pending inserting the new task to the innerHTML and displaying it

--- script.js
+++ script.js
@@ -1,18 +1,129 @@
-const filterButtons = document.querySelectorAll('.filterButton');
+const filterBtns = document.querySelectorAll('.filterButton');
+const newTaskBtn = document.getElementById('newTaskButton');
 
 //Itering trough em
-filterButtons.forEach(btn => {
+filterBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         //Desactivating
-        desactivatingActive(filterButtons);
+        removeArrayActive(filterBtns);
 
         btn.classList.add("active")
     })
 });
 
+//Overlay const
+const overlay = document.getElementById('overlay')
 
-function desactivatingActive(array){
+//Listener for the newTaskButton:
+newTaskBtn.addEventListener('click', () => {
+    overlay.classList.add('active');
+});
+
+//Listener for closing the overlay:
+const closeButton = document.getElementById('closeOverlay')
+
+closeButton.addEventListener('click', () => {
+    closeModal();
+});
+overlay.addEventListener('click', (e) => {
+    if (e.target === overlay){
+        closeModal();
+    };
+});
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+       closeModal();
+    }
+});
+
+//FOR THE BUTTONS INSIDE THE SELECTORS
+const statusSelector = document.querySelectorAll('.statusButton')
+const prioritySelector = document.querySelectorAll('.priorityButton')
+
+statusSelector.forEach(btn => {
+    btn.addEventListener('click', () =>{
+        removeArrayActive(statusSelector);
+        btn.classList.add('active');
+    });
+});
+
+prioritySelector.forEach(btn => {
+    btn.addEventListener('click', () =>{
+        removeArrayActive(prioritySelector);
+        btn.classList.add('active');
+    });
+});
+
+//Form, reset, submit
+const formTask = document.getElementById('taskFormId');
+const resetButton = document.getElementById('resetButton')
+const submitButton = document.getElementById('submitButton')
+
+//CREATING DATA STORAGER
+let tasksArrayData = [];
+
+//CREATING RESET BUTTON:
+resetButton.addEventListener('click', ()=>{
+    resetForm();
+});
+
+function resetForm(){
+    // Resetting Values
+    formTask.reset();
+
+    // Resetting the custom Buttons
+    removeArrayActive(statusSelector);
+    removeArrayActive(prioritySelector);
+
+    // Adding the defaults
+    document.getElementById('inProgressStatus').classList.add('active');
+    document.getElementById('mediumPriority').classList.add('active');
+}
+
+//Getting the form info
+formTask.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const title = document.getElementById('titleInput').value;
+    const description = document.getElementById('descriptionInput').value;
+    const dueDate = document.getElementById('dueDateInput').value;
+    const status = document.querySelector('.statusButton.active').textContent;
+    const priority = document.querySelector('.priorityButton.active').textContent;
+    const percentage = document.getElementById('percentageInput').value;
+
+    //CREATING THE DATA:
+    const task = {
+        id: Date.now(),
+        title,
+        description,
+        dueDate,
+        status,
+        priority,
+        percentage: Number(percentage)
+    }
+
+    console.log(task);
+
+    //SUBMITTING THE TASK TO THE ARRAY OF TASKS
+    tasksArrayData.push(task);
+    console.log(tasksArrayData);
+
+
+    //Closing the overlay modal
+    closeModal();
+});
+
+function closeModal(){
+    overlay.classList.remove('active')
+    resetForm();
+}
+
+function removeArrayActive(array){
     array.forEach(element => {
         element.classList.remove("active")
     });
+}
+
+function removeElementActive(element){
+    element.classList.remove("active")
 }
